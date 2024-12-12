f11890e1d353acf5b84d7eb4df9f9b3eabf8cd9f
anhtran <anhtr077@gmail.com>
2024-12-12 12:21:18 -0800

    Update oncomplete and wworking in buttonclass

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,17 +1,20 @@
-import { useState } from 'react';
+// import { useState } from 'react';
 import PropTypes from 'prop-types';
 
 import './Task.css';
 
-const Task = ({ id, title, isComplete }) => {
-  const [complete, setComplete] = useState(isComplete);
-  const buttonClass = complete ? 'tasks__item__toggle--completed' : '';
+const Task = ({ id, title, isComplete, onComplete }) => {
+  // const [complete, setComplete] = useState(isComplete);
 
+  const buttonClass = isComplete ? 'tasks__item__toggle--completed' : '';
+  const taskClick = () => {
+    onComplete(id);
+  };
   return (
     <li className="tasks__item">
       <button
         className={`tasks__item__toggle ${buttonClass}`}
-        onClick={() => setComplete(!complete)}
+        onClick={taskClick}
       >
         {title}
       </button>
@@ -24,6 +27,7 @@ Task.propTypes = {
   id: PropTypes.number.isRequired,
   title: PropTypes.string.isRequired,
   isComplete: PropTypes.bool.isRequired,
+  onComplete: PropTypes.func.isRequired,
 };
 
 export default Task;
